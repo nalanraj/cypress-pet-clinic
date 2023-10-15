a1ed90e9d33e990ffc592d10df5364ae7ef6500c
Nalan <nalanraj@gmail.com>
2023-10-16 00:37:14 +0200

    new version update 13

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    saveJson: true,
+    reportPageTitle: 'E2E Test Report',
+    html: {
+      enabled: true,
+    },
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: 'http://localhost:8080',
+  },
+})
